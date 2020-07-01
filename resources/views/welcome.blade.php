0945eecf7ebab1a1f6c1b6a24978140dab7ebcc2
Gael <gaelpennerad@gmail.com>
2020-07-01 10:07:16 +0200

    integration page d'accueil css image

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,104 +1,301 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!doctype html>
+<html lang="fr">
 
-        <title>Laravel</title>
+<head>
+    <title>Association Les Randounaires de Cassagnas &mdash; Website by CarreJaune</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans:400,700,900&display=swap" rel="stylesheet">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
 
-            .full-height {
-                height: 100vh;
-            }
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="css/aos.css">
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+    <!-- MAIN CSS -->
+    <link rel="stylesheet" href="css/style.css">
 
-            .position-ref {
-                position: relative;
-            }
+</head>
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+<body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
 
-            .content {
-                text-align: center;
-            }
 
-            .title {
-                font-size: 84px;
-            }
+<div class="site-wrap" id="home-section">
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+    <div class="site-mobile-menu site-navbar-target">
+        <div class="site-mobile-menu-header">
+            <div class="site-mobile-menu-close mt-3">
+                <span class="icon-close2 js-menu-toggle"></span>
+            </div>
+        </div>
+        <div class="site-mobile-menu-body"></div>
+    </div>
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                    @foreach($test as $one)
-                        {{$one->Nom_User}}
+    <header class="site-navbar site-navbar-target" role="banner">
 
-                    @endforeach
+        <div class="container">
+            <div class="row align-items-center position-relative">
+
+                <div class="col-3 ">
+                    <div class="site-logo">
+                        <a href="index.html" class="font-weight-bold">
+                            <img src="images/logo.png" alt="Image" >
+                        </a>
+                    </div>
+                    <div>Les Randounaires de Cassagnas</div>
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog de </a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="col-9  text-right">
+
+
+              <span class="d-inline-block d-lg-none">
+                <a href="#" class="text-white site-menu-toggle js-menu-toggle py-5 text-white">
+                  <span class="icon-menu h3 text-white"></span></a></span>
+
+
+
+                    <nav class="site-navigation text-right ml-auto d-none d-lg-block" role="navigation">
+                        <ul class="site-menu main-menu js-clone-nav ml-auto ">
+                            <li class="active"><a href="index.html" class="nav-link">Acceuil</a></li>
+                            <li><a href="trips.html" class="nav-link">Programme</a></li>
+                            <li><a href="about.html" class="nav-link">A propos de nous</a></li>
+                            <li><a href="picture.html" class="nav-link">Galerie Photo</a></li>
+                            <li><a href="press.html" class="nav-link">Presse</a></li>
+                            <li><a href="contact.html" class="nav-link">Contact</a></li>
+                        </ul>
+                    </nav>
                 </div>
             </div>
         </div>
-    </body>
+
+    </header>
+
+    <div class="ftco-blocks-cover-1">
+        <div class="site-section-cover overlay" style="background-image: url('images/hero_1.jpg')">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-md-5" data-aos="fade-right">
+                        <h1 class="mb-3 text-white">Profitons des merveilles de la nature</h1>
+                        <p>L'association vous propose de découvrir la Lozère au travers de randonnées pédestres conviviales et à la portée de chacun.
+                        </p>
+                        <p class="d-flex align-items-center">
+                            <!-- <a href="https://vimeo.com/191947042" data-fancybox class="play-btn-39282 mr-3"><span class="icon-play"></span></a>
+                            <span class="small">Watch the video</span> -->
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="site-section py-5">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-6">
+                    <div class="heading-39101 mb-5">
+                        <span class="backdrop">Notre histoire</span>
+                        <span class="subtitle-39191">Découvrez notre histoire</span>
+                        <h3>Notre Histoire</h3>
+                    </div>
+                    L'association les Randounaïres de Cassagnas, fondée le 26 septembre 2009, a pour objet la pratique et le développement de la randonnée pédestre, tant pour sa pratique sportive que pour la découverte et la sauvegarde de l’environnement, le tourisme et les loisirs.
+                </div>
+                <div class="col-md-6" data-aos="fade-right">
+                    <img src="images/traveler.jpg" alt="Image" class="img-fluid">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="site-section">
+
+        <div class="container">
+
+            <div class="row justify-content-center text-center">
+                <div class="col-md-10">
+                    <div class="heading-39101 mb-5">
+                        <span class="backdrop text-center">COMMENTAIRES</span>
+                        <span class="subtitle-39191">COMMENTAIRES</span>
+                        <h3>Témoignages</h3>
+                    </div>
+                </div>
+            </div>
+
+            <div class="owl-carousel slide-one-item">
+                <div class="row">
+                    <div class="col-md-6">
+
+                        <div class="testimonial-39191 d-flex">
+                            <div class="mr-4">
+                                <img src="images/person_1.jpg" alt="Image" class="img-fluid">
+                            </div>
+                            <div>
+                                <blockquote>&ldquo;Je suis content d'avoir participer à cette randonées avec…&rdquo;</blockquote>
+                                <p>&mdash; John Doe</p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="col-md-6">
+
+                        <div class="testimonial-39191 d-flex">
+                            <div class="mr-4">
+                                <img src="images/person_2.jpg" alt="Image" class="img-fluid">
+                            </div>
+                            <div>
+                                <blockquote>&ldquo;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, accusamus, facilis! Placeat praesentium alias porro aperiam facilis accusantium veniam?&rdquo;</blockquote>
+                                <p>&mdash; John Doe</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-6">
+
+                        <div class="testimonial-39191 d-flex">
+                            <div class="mr-4">
+                                <img src="images/person_1.jpg" alt="Image" class="img-fluid">
+                            </div>
+                            <div>
+                                <blockquote>&ldquo;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, accusamus, facilis! Placeat praesentium alias porro aperiam facilis accusantium veniam?&rdquo;</blockquote>
+                                <p>&mdash; John Doe</p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="col-md-6">
+
+                        <div class="testimonial-39191 d-flex">
+                            <div class="mr-4">
+                                <img src="images/person_2.jpg" alt="Image" class="img-fluid">
+                            </div>
+                            <div>
+                                <blockquote>&ldquo;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, accusamus, facilis! Placeat praesentium alias porro aperiam facilis accusantium veniam?&rdquo;</blockquote>
+                                <p>&mdash; John Doe</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+
+
+    <div class="site-section bg-image overlay" style="background-image: url('images/hero_1.jpg')">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-7 text-center">
+                    <h2 class="font-weight-bold text-white">Pour nous rejoindre, contacter nous !</h2>
+                    <p class="text-white">Besoin de renseignement ? Vous voulez partager l'expérience ?</p>
+                    <p class="mb-0"><a href="contact.html" class="btn btn-primary text-white py-3 px-4">Contactez nous</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer class="site-footer bg-light">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-3">
+                    <h2 class="footer-heading mb-3">Partenaires</h2>
+                    <div class="row">
+                        <div class="col-4 gal_col">
+                            <a href="#"><img src="images/insta_1.jpg" alt="Image" class="img-fluid"></a>
+                        </div>
+                        <div class="col-4 gal_col">
+                            <a href="#"><img src="images/insta_2.jpg" alt="Image" class="img-fluid"></a>
+                        </div>
+                        <div class="col-4 gal_col">
+                            <a href="#"><img src="images/insta_3.jpg" alt="Image" class="img-fluid"></a>
+                        </div>
+                        <div class="col-4 gal_col">
+                            <a href="#"><img src="images/insta_4.jpg" alt="Image" class="img-fluid"></a>
+                        </div>
+                        <div class="col-4 gal_col">
+                            <a href="#"><img src="images/insta_5.jpg" alt="Image" class="img-fluid"></a>
+                        </div>
+                        <div class="col-4 gal_col">
+                            <a href="#"><img src="images/insta_6.jpg" alt="Image" class="img-fluid"></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-8 ml-auto">
+                    <div class="row">
+                        <div class="col-lg-6 ml-auto">
+                            <h2 class="footer-heading mb-4">Liens utiles</h2>
+                            <ul class="list-unstyled">
+                                <li><a href="documents/Statuts  de l’association Les Randounaires de Cassagnas.docx">Statuts</a></li>
+                                <li><a href="documents/Statuts  de l’association Les Randounaires de Cassagnas.docx">compte rendu</a></li>
+                                <li><a href="documents/reglement interieur adopté AG 14 10 2017.docx">réglement intérieur</a></li>
+                                <li><a href="cg.html">Condition générale</a></li>
+                                <li><a href="contact.html">Contact</a></li>
+                            </ul>
+                        </div>
+
+                        <div class="col-lg-6">
+                            <h2 class="footer-heading mb-4">Newsletter</h2>
+                            <p>Pour recevoir des nouvelles de nous, laissez-nous votre mail !</p>
+                            <form action="#" class="d-flex" class="subscribe">
+                                <input type="text" class="form-control mr-3" placeholder="Email">
+                                <input type="submit" value="Send" class="btn btn-primary">
+                            </form>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <div class="row pt-5 mt-5 text-center">
+                <div class="col-md-12">
+                    <div class="border-top pt-5">
+                        <p>
+                            <!-- carrejaune -->
+                            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Create for association 1901 <i class="icon-heart text-danger" aria-hidden="true"></i> : <a href="#" target="_blank" >RANDONNAILLRE</a>
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </footer>
+
+</div>
+
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/jquery-migrate-3.0.0.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/jquery.sticky.js"></script>
+<script src="js/jquery.waypoints.min.js"></script>
+<script src="js/jquery.animateNumber.min.js"></script>
+<script src="js/jquery.fancybox.min.js"></script>
+<script src="js/jquery.stellar.min.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="js/bootstrap-datepicker.min.js"></script>
+<script src="js/isotope.pkgd.min.js"></script>
+<script src="js/aos.js"></script>
+
+<script src="js/main.js"></script>
+
+</body>
+
 </html>
+
+ 
